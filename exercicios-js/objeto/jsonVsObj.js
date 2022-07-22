fdd948a395be5bff53d005c1745218c2002cf958
Daniel <filhonando012@gmail.com>
2022-07-21 21:50:27 -0300

    modif jsonVsObj

--- exercicios-js/objeto/jsonVsObj.js
+++ exercicios-js/objeto/jsonVsObj.js
@@ -3,3 +3,4 @@ const obj = {a: 1, b: 2, c: 3, soma() { return a + b + c } }
 console.log(JSON.stringify(obj)) // Objeto > JSON
 //console.log(JSON.parse("{ a: 1, b: 2, c: 3 }"))
 console.log(JSON.parse('{ "a": 1, "b": 2, "c": 3 } ')) // JSON > Objeto
+console.log(JSON.parse('{ "a": 1, "b": "string", "c": true, "d": {}, "e": [] }'))
