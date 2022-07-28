ebd74d316f3b67d1e69edc9b341f66e9f8f7c03c
Daniel <filhonando012@gmail.com>
2022-07-27 22:40:08 -0300

    modificando array

--- exercicios-js/array/array.js
+++ exercicios-js/array/array.js
@@ -10,4 +10,21 @@ console.log(aprovados[1])
 console.log(aprovados[2])
 console.log(aprovados[3])
 
-aprovados[3] ='Paulo'
+aprovados[3] ='Paulo' // melhor usado para substituir um elemento ja existente
+aprovados.push('Abia')
+console.log(aprovados.length)
+aprovados[9] = 'Rafael'
+console.log(aprovados.length)
+
+console.log(aprovados[8] === undefined)
+
+console.log(aprovados)
+aprovados.sort()
+console.log(aprovados)
+
+delete aprovados[1]
+console.log(aprovados[2])
+
+aprovados = ['Bia','Carlos', 'Ana']
+aprovados.splice(1,1, ) // add elementos ou del 
+console.log(aprovados)
