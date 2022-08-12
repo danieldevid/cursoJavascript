9e9440413bb81e4932682fabd081956958bf5637
Daniel <filhonando012@gmail.com>
2022-08-11 21:38:10 -0300

    add concat

---
+++ exercicios-js/array/concat.js
@@ -0,0 +1,5 @@
+const filhas = ['Uleskah', 'Cibalena']
+const filhos = ['Uoxiton', 'Uesclei']
+const todas = filhas.concat(filhos, 'Fulano')
+console.log(todas, filhas, filhos)
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
