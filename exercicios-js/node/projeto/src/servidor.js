f78086f4bf033f467046894cadf9458aba49ae79
Daniel <filhonando012@gmail.com>
2022-09-09 22:36:07 -0300

    add modificando servidor

--- exercicios-js/node/projeto/src/servidor.js
+++ exercicios-js/node/projeto/src/servidor.js
@@ -4,5 +4,10 @@ const express = require('express')
 const app = express() 
 
 app.get('/produtos', (req, res, next) => {
-    res.send({ nome: 'Notebook', preco: 123.45 })
+    res.send({ nome: 'Notebook', preco: 123.45 })// COnverte para Json
+    
+})
+
+app.listen(porta, () => {
+    console.log(` Servidor executando está porta ${porta}.`)
 })
