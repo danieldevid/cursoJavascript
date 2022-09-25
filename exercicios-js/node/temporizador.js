8d5441237c87994561721da8728673b09a9c71ba
Daniel <filhonando012@gmail.com>
2022-09-25 08:54:09 -0300

    add temporizador

--- exercicios-js/node/temporizador.js
+++ exercicios-js/node/temporizador.js
@@ -1,6 +1,6 @@
 const schedule = require('node-schedule')
 
-const tarefa1 = schedule.scheduleJob('*/5 * 19 * * 6', function() {
+const tarefa1 = schedule.scheduleJob('*/5 * 8 * * 7', function() {
     console.log('Executando Tarefa 1!', new Date().getSeconds())
 })
 
@@ -10,6 +10,13 @@ setTimeout(function() {
 }, 20000)
 
 const regra = new schedule.RecurrenceRule()
-regra.dayOfWeek = [new schedule.Range(1, 5)]
-regra.hour = 12
-regra.second = 30 
+regra.dayOfWeek = [new schedule.Range(0, 6)]
+regra.hour = 8
+regra.second = 30
+
+// setImmediate
+// setInterval
+
+const tarefa2 = schedule.scheduleJob( regra, function () {
+    console.log('Executando Tarefa 2!', new Date().getSeconds())
+})
