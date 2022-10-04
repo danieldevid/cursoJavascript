96663264e486ab18f9e953e49ec7065048e11215
Daniel <filhonando012@gmail.com>
2022-10-03 21:56:17 -0300

    add revisão

---
+++ exercicios-js/esnext/revisão1.js
@@ -0,0 +1,15 @@
+// let  const 
+
+{
+    var a = 2
+    let b = 3
+    
+}
+
+console.log(a) 
+// variavel acoplada a um var não tem 
+// escopo de bloco 
+//console.log(b) // O contrario acontece aqui 
+      //  Template String
+const produto = 'iPad'
+console.log(`${produto} é caro `)
