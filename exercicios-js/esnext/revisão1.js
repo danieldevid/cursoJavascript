fe6217e5f7344c895912cd0bf0906aecc39a8e62
Daniel <filhonando012@gmail.com>
2022-10-18 21:16:58 -0300

    add revisao 1

--- exercicios-js/esnext/revisão1.js
+++ exercicios-js/esnext/revisão1.js
@@ -13,3 +13,12 @@ console.log(a)
       //  Template String
 const produto = 'iPad'
 console.log(`${produto} é caro `)
+ // Destructuring
+const [l,e, ...tras] = "Cod3r"
+console.log(l, e, tras)
+
+const [x, y] = [1, 2]
+console.log(x,y)
+
+const { idade: i, nome } = { nome:'Ana', idade: 9}
+console.log(i, nome )
