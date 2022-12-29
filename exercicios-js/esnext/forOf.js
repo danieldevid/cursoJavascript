156338062bdc015c51943627e56b71a068a9ed37
Daniel <filhonando012@gmail.com>
2022-12-28 22:33:10 -0300

    add for of

--- exercicios-js/esnext/forOf.js
+++ exercicios-js/esnext/forOf.js
@@ -21,3 +21,19 @@ const assuntosMap =new Map([
 for (let assunto of assuntosMap) {
     console.log(assunto)
 }
+
+for (let chave of assuntosMap.keys() ) {
+    console.log(chave)
+}
+
+for ( let valor of assuntosMap.values()) {
+    console.log(valor)
+}
+for ( let [ ch, vl ] of assuntosMap.entries() ) {
+    console.log(ch, vl)
+}
+
+const s = new Set(['a','b','c'])
+for (let letra of s) {
+    console.log(letra)
+}
