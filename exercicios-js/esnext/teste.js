40800d37828c09968ba2194ceec56b5cf77a52d9
Daniel <filhonando012@gmail.com>
2023-01-16 22:24:33 -0300

    add revisao4

--- exercicios-js/esnext/teste.js
+++ exercicios-js/esnext/teste.js
@@ -10,11 +10,19 @@ console.log(total(1,3,4,5))
 
 
 // Class 
- class Animal {}
- class Cachorro extends Animal {
-   falar () {
-      return 'Au Au Au '
+class Animal {}
+class Cachorro extends Animal {
+   falar() {
+      return 'Au AU AU '
    }
- }
+}
+console.log(new Cachorro().falar())
 
- console.log(new Cachorro().falar())
+// usar spread com objeto
+const funcionario = { nome:'Maria', salario: 1234.56 }
+const clone = { ativo:true, ...funcionario }
+console.log(clone)
+// usar spread com Array 
+const grupoA = [ 'Daniel', 'Alan', 'Silvio']
+const grupoFinal =  [ 'João', ...grupoA, 'Emersom']
+console.log(grupoFinal)
