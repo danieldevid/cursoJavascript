04f22e524ad561146d884ef7af523057b85dfc7a
Daniel <filhonando012@gmail.com>
2023-02-02 20:46:52 -0300

    add prom

--- exercicios-js/esnext/promise.js
+++ exercicios-js/esnext/promise.js
@@ -1,5 +1,12 @@
 function falarDepoisDe(segundos, frase) {
-    return new Promise ( resolve => {
-        
-    } )
+    return new Promise ((resolve, reject ) => {
+        setTimeout(() => {
+            resolve(frase)
+        },segundos*1000)
+    }) 
 }
+
+falarDepoisDe(3, 'TA COMPLICADO!!')
+    .then(frase =>  frase.concat('??!!'))
+    .then(Outrafrase => console.log(Outrafrase))
+    .catch( e => console.log(e))
